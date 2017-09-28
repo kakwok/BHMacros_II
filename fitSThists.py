619edd4b76e0a423669200650d0f267a4077adfa
Martin <ka_hei_martin_kwok@brown.edu>
2017-09-28 17:48:03 +0200

    Add norm uncertainty to dataCards

--- fitSThists.py
+++ fitSThists.py
@@ -52,7 +52,7 @@ fitNormRanges = FitAndNormRange(argv[3])
 fitNormRanges.showFitRanges()
 fitNormRanges.showNormRanges()
 rebin          = False   # Rebin from 50GeV to 100GeV
-WriteDataCards = True 
+WriteDataCards = False 
 DrawUncertainty= True 
 #f_outlier     = null
 
@@ -209,6 +209,19 @@ def getEnvelopeFunctions(bestfit, functions, xlow, xup, mode):
     fup  = TF1("fUp_symmetrized",fUpFormula,xlow,xup)
     return (flow,fup)
 
+#Use trapezoidal rule to inegrate a TGraph
+def GraphIntegrate(g, a, b, n):
+    h = float(b - a) / n
+    s = 0.0
+    #if (a>8000): print a, g.Eval(a,0,"S")/2.0
+    s += g.Eval(a,0,"S")/2.0
+    for i in range(1, n):
+    #    if (a+i*h>8000): print a+i*h, g.Eval(a+i*h,0,"S")
+        s += g.Eval(a + i*h,0,"S")
+    s += g.Eval(b,0,"S")/2.0
+    #if (a>8000): print b,g.Eval(b,0,"S")/2.0
+    return s * h
+
 # Add normalization error to fLow and fUp, return as TGraph. normErr given in fraction.
 def getNormErrorGraphs(fLow,fUp,fbest, normErr,RelOrAbs):
     fLow_norm = TGraph()
@@ -229,9 +242,15 @@ def getNormErrorGraphs(fLow,fUp,fbest, normErr,RelOrAbs):
         norm_err  = fbest.Eval(x)* normErr
         delta     = sqrt( norm_err**2 + shape_err**2)
         if RelOrAbs=="Abs":
-            fLow_norm.SetPoint(fLow_norm.GetN(), x, fbest.Eval(x)-delta )
+            if (delta<fbest.Eval(x)):
+                fLow_norm.SetPoint(fLow_norm.GetN(), x, fbest.Eval(x)-delta ) 
+            else:
+                fLow_norm.SetPoint(fLow_norm.GetN(), x, 0 )  # fLow_norm should not be negative
         elif RelOrAbs=="Rel":
-            fLow_norm.SetPoint(fLow_norm.GetN(), x, -1*delta/fbest.Eval(x) )
+            if (delta<fbest.Eval(x)):
+                fLow_norm.SetPoint(fLow_norm.GetN(), x, -1*delta/fbest.Eval(x) )
+            else:
+                fLow_norm.SetPoint(fLow_norm.GetN(), x, 0 )  # fLow_norm should not be negative
     return fLow_norm, fUp_norm
 
 # Return the TGraph bounded by fLow and fUp for drawing
@@ -315,7 +334,7 @@ def getNormalizedFunctionWithChi2(f, hist, ExcOrInc, j, STlow=0, STup=0):
     FracNormErr    = sqrt(1.0/sqrt(histBinTotal)+1.0/sqrt(normBinTotal))
     NormErr        = FracNormErr * normfactor 
     if(ExcOrInc=="Inc" and not(j in NormTable_j) and ("exc3" in f.GetName())):
-        NormTable.append([">=%s"%j,"%s-%s"%(LowerNormBound/1000,UpperNormBound/1000),"%.3f+-%.3f"%(normfactor,NormErr),"%.3f"%(FracNormErr*100)+"%"])
+        NormTable.append([">=%s"%j,"%s-%s"%(LowerNormBound/1000,UpperNormBound/1000),"%.3f+-%.3f"%(normfactor,NormErr),"%.1f"%(FracNormErr*100)+"%"])
         NormFactors["Inc%s"%j]    = normfactor
         FracNormErrors["Inc%s"%j] = FracNormErr 
         NormTable_j.append(j)
@@ -411,13 +430,13 @@ def PickBestFit(fdict):
     print fvalues
     print fdict
     fbestValue = 0
-    # pick the value of closst to median-mean
-    for f in fvalues:
-        if (f-median)>(-1*mean):
-            fbestValue = f
-            break
+    fvalues = abs( fvalues - (median+mean)/2)
+    #fbestValue is the min abs(deviation from median+mean/2
+    fbestValue = min(fvalues)
+    
+    # Find the best value in fdict
     for fname in fdict:
-        if fdict[fname]==fbestValue: fbestName = fname
+        if (fdict[fname]==((median+mean)/2+fbestValue) or fdict[fname]==((median+mean)/2-fbestValue)): fbestName = fname
     return fbestName
     
 def setBestFit(functions):
@@ -482,6 +501,7 @@ def NormAndDrawST(stHist,j,ExcOrInc,stRefHist,WriteCanvas):
     
     stHist.GetXaxis().SetLabelSize(0)
     stHist.SetMinimum(1e-1)
+    #stHist.SetMinimum(1e-4)
 
     print "in N=%i, upperNormEdge = %s, lowerNormEdge = %s" % (j,upperNormEdge, lowerNormEdge)
     chi2_list     = []
@@ -517,13 +537,14 @@ def NormAndDrawST(stHist,j,ExcOrInc,stRefHist,WriteCanvas):
     print "-----------------------------------------"
     print "In N=%i, fbest is chosen to be %s\n"%(j,fbest.GetName())
 
-    fLow,fUp  = getEnvelopeFunctions( fbest, functions, 2500, 7500, "shade")
+    fLow,fUp  = getEnvelopeFunctions( fbest, functions, 2500, 11000, "shade")
     fillGraph = getFillGraph( fLow, fUp )
     if (ExcOrInc=="Exc"):
         fLow_norm,fUp_norm= getNormErrorGraphs( fLow, fUp, fbest, 0 ,"Abs")
     if (ExcOrInc=="Inc"):
         print "In N=%i, fractional errors to be %s\n"%(j,FracNormErrors["Inc%s"%j])
         fLow_norm,fUp_norm= getNormErrorGraphs( fLow, fUp, fbest, FracNormErrors["Inc%s"%j] ,"Abs")
+        #fLow_norm,fUp_norm= getNormErrorGraphs( fLow, fUp, fbest, 0 ,"Abs")
     
     if DrawUncertainty:
         fillGraph.SetFillColorAlpha(kGray,0.35)
@@ -582,8 +603,8 @@ def NormAndDrawST(stHist,j,ExcOrInc,stRefHist,WriteCanvas):
         else:
             legend.AddEntry(stHist,"Data: multiplicity >=%i"%j,"ep");
     if DrawUncertainty:
-	    legend.AddEntry(fillGraph,"Background from fit","fl");
-	    legend.AddEntry(fLow_norm,"Normalization uncertainty","l");
+	    legend.AddEntry(fillGraph,"Background Shape","fl");
+	    legend.AddEntry(fLow_norm,"Systematic Uncertainties","l");
     legend.Draw()
 
     #CMS_lumi.CMS_lumi(STcomparisons[canvasName], iPeriod, iPos)
@@ -673,17 +694,24 @@ def NormAndDrawST(stHist,j,ExcOrInc,stRefHist,WriteCanvas):
 
     if (WriteDataCards and ExcOrInc=="Inc"):
         outputForLimits = open("output/%s_Inclusive%i.txt"%(argv[2].replace(".root",""),j), "w")
-        outputForLimits.write("STMin::Observed_Data::Expected_Bkg::Shape_Unc\n")
+        outputForLimits.write("STMin::Observed_Data::Expected_Bkg::Shape_Unc_down::Shap_Unc_up::Norm_Unc\n")
         for stmin in range(25, 90):
             observed=0
             startbin=stHist.GetXaxis().FindBin(float(stmin*100))
             for stbin in range (startbin, stHist.GetXaxis().GetNbins()):
                 observed+=stHist.GetBinContent(stbin)
             expected = fbest.Integral(stmin*100, 11000)/binwidth
-            shapeUnc = abs(fLow.Integral(stmin*100, 11000)/binwidth-expected)/expected +1
-            #print stmin*100, shapeUnc, expected
+            #shapeUnc = (fLow.Integral(stmin*100, 11000)/binwidth)/expected
+            #shapeUnc = abs(fLow.Integral(stmin*100, 11000)/binwidth-expected)/expected +1
+            #shapeUnc = abs(fUp.Integral(stmin*100, 11000)/binwidth-expected)/expected +1
+
+            #format for datacard for using asymmetric log-normal
+            shapeNormUncLow = (GraphIntegrate(fLow_norm,stmin*100, 11000,(11000-stmin*100)/50)/binwidth) /expected 
+            shapeNormUncUp  = (GraphIntegrate(fUp_norm,stmin*100, 11000,(11000-stmin*100)/50)/binwidth)  /expected
+            #print stmin*100, shapeUnc, shapeNormUncLow, expected
+            #print stmin*100, fLow.Eval(stmin*100), fLow_norm.Eval(stmin*100)
             if not ((j>5 and stmin<23) or (j>8 and stmin<25) or (j>10 and stmin<26)):
-                outputForLimits.write("%i  %i  %f  %f\n" % (stmin*100, observed, expected, shapeUnc))
+                outputForLimits.write("%i  %i  %f  %f %f\n" % (stmin*100, observed, expected, shapeNormUncLow, shapeNormUncUp))
         outputForLimits.close()
 
 
@@ -729,38 +757,48 @@ ATLASBH6_string="([0]*(1-x/13000)^[1])*((1+x/13000)**([2]*(x/13000)))"          
 #UA23_string     ="[0]*(x/13000)^([1])*TMath::Exp(1)^([2]*(x/13000)+[3]*(x/13000)^2+[4]*(x/13000)^3)"
 #dijetMod        ="([0]*(1-x/13000)^[1])*(1+[2]*(x/13000))/((x/13000)**([2]+[3]*TMath::Log(x/13000)))"
 
-
 # Define dictionaries of functions for fitting different histograms
-#fnames = {"f1":f1_string,"f2":f2_string,"f3":f3_string,"f4":f4_string,"f5":f5_string}
-#fnames = {"f1":f1_string,"f2":f2_string,"f4":f4_string}
-#fnames.update({"VVdijet2":VVdijet2_string,"VVdijet1":VVdijet1_string})
-#fnames.update({"ATLAS1":ATLAS1_string,"ATLAS2":ATLAS2_string})
-#fnames.update({"dijet2":dijet2_string,"dijet3":dijet3_string})
-#fnames.update({"exPow":exPow_string})
-fnames = {
-"CMSBH1":CMSBH1_string,
-"CMSBH2":CMSBH2_string,
-"dijet1":dijet1_string,
-"dijet2":dijet2_string,
-#"dijet3":dijet3_string,
-"ATLAS1":ATLAS1_string,
-"ATLAS2":ATLAS2_string,
-"UA21":UA21_string,
-"UA22":UA22_string,
-#"UA23":UA23_string,
-#"dijetMod":dijetMod,
-"ATLASBH1":ATLASBH1_string,
-"ATLASBH2":ATLASBH2_string,
-"ATLASBH3":ATLASBH3_string,
-"ATLASBH4":ATLASBH4_string,
-"ATLASBH5":ATLASBH5_string,
-"ATLASBH6":ATLASBH6_string
-}
+if("QCD" in PlotsFname):
+    fnames = {
+        #"CMSBH1":CMSBH1_string,
+        #"CMSBH2":CMSBH2_string,
+        "dijet1":dijet1_string,
+        "dijet2":dijet2_string,
+        #"dijet3":dijet3_string,
+        "ATLAS1":ATLAS1_string,
+        "ATLAS2":ATLAS2_string,
+        #"UA21":UA21_string,
+        "UA22":UA22_string,
+        #"UA23":UA23_string,
+        #"dijetMod":dijetMod,
+        "ATLASBH1":ATLASBH1_string,
+        #"ATLASBH2":ATLASBH2_string,
+        #"ATLASBH3":ATLASBH3_string,
+        "ATLASBH4":ATLASBH4_string,
+        "ATLASBH5":ATLASBH5_string,
+        #"ATLASBH6":ATLASBH6_string
+    }
+if("data10percent" in PlotsFname):
+    fnames = {
+        "CMSBH1":CMSBH1_string,
+        "CMSBH2":CMSBH2_string,
+        "dijet1":dijet1_string,
+        "dijet2":dijet2_string,
+        #"dijet3":dijet3_string,
+        "ATLAS1":ATLAS1_string,
+        "ATLAS2":ATLAS2_string,
+        #"UA21":UA21_string,
+        #"UA22":UA22_string,
+        #"UA23":UA23_string,
+        #"dijetMod":dijetMod,
+        "ATLASBH1":ATLASBH1_string,
+        "ATLASBH2":ATLASBH2_string,
+        "ATLASBH3":ATLASBH3_string,
+        "ATLASBH4":ATLASBH4_string,
+        "ATLASBH5":ATLASBH5_string,
+        "ATLASBH6":ATLASBH6_string
+    }
 
-#fnames = {"VVdijet2":VVdijet2_string,"VVdijet1":VVdijet1_string}
-#fnames.update({"ATLAS1":ATLAS1_string,"ATLAS2":ATLAS2_string})
-#fnames = {"dijet2":dijet2_string,"dijet3":dijet3_string}
-#fnames.update({"exPow":exPow_string})
 for fname in fnames:
     f2_list[fname+"_exc2"]     = TF1(fname+"_exc2",fnames[fname],1000,STup)
     f3_list[fname+"_exc3"]     = TF1(fname+"_exc3",fnames[fname],1000,STup)
@@ -768,7 +806,6 @@ for fname in fnames:
     chi2graphs_fit[fname] =TGraph()
 
 AllFitList=[f3_list,f4_list]
-#AllFitList=[f3_list]
 
 for flist in AllFitList:
     for fname in sorted(flist.iterkeys()):
@@ -1018,8 +1055,12 @@ leg.SetFillColor(0);
 leg.Draw()
 print tabulate(chi2Table,"firstrow")
 print tabulate(NormTable,"firstrow")
-print "median_integral={} mean_integral={}".format(np.median(np.array(f_integrals.values())), np.mean(np.array(f_integrals.values())))
+mean = np.mean(np.array(f_integrals.values()))
+median = np.median(np.array(f_integrals.values()))
+
+print "median_integral={} mean_integral={} ave={}".format(median, mean, (median+mean)/2)
 print np.sort(np.array(f_integrals.values()))
+print abs(np.sort(np.array(f_integrals.values()))-(median+mean)/2)
 #for row in chi2Table:
 #    print row
 c1.Write()
